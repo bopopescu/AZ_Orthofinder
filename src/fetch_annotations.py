94526a8971f6840dc2dd751cad1892f5d551bb22
vladsaveliev <vladsaveliev@me.com>
2014-01-30 15:06:18 +0400

    Excluding partial

--- src/fetch_annotations.py
+++ src/fetch_annotations.py
@@ -147,7 +147,8 @@ def fetch_annotations_species_name_entrez(save_dir, species_names, clip=100000):
         if species_name == '' or species_name[0] == '#':
             continue
 
-        term = '(%s[Organism] AND (complete genome[Title] OR complete sequence[Title]))' % species_name
+        term = '(%s[Organism] AND (complete genome[Title] OR ' \
+               'complete sequence[Title])) NOT (partial[Title])' % species_name
         log.info('   Query: %s' % term)
         search_handle = Entrez.esearch(db='nuccore', retmax=100000, term=term)
         ids = Entrez.read(search_handle)['IdList']

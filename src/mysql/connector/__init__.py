6c4be4f9df6414c2093d995efa5b8b96c77ddffd
vladsaveliev <vladsaveliev@me.com>
2014-02-04 18:05:14 +0400

    Connection addsitedir dirname

--- src/mysql/connector/__init__.py
+++ src/mysql/connector/__init__.py
@@ -29,7 +29,7 @@ from site import addsitedir
 from os.path import join, realpath, dirname
 import sys
 addsitedir(join(dirname(realpath(__file__)), '..', '..'))
-#sys.path.append(join(dirname(realpath(__file__)), '../../'))
+sys.path.append(join(dirname(realpath(__file__)), '../../'))
 
 from mysql.connector.connection import MySQLConnection
 from mysql.connector.errors import (  # pylint: disable=W0622

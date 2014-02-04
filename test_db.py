deb6a62421b7bc85e5df072fa455f72f8862f905
vladsaveliev <vladsaveliev@me.com>
2014-02-04 20:03:25 +0400

    Connecting as root; Also test_db.py takes parameters from config.txt

--- test_db.py
+++ test_db.py
@@ -2,6 +2,7 @@
 from os.path import join, realpath
 
 import traceback
+from src import config
 from src.mysql import connector
 from src.mysql.connector import errorcode
 
@@ -9,12 +10,19 @@ import sys
 from site import addsitedir
 addsitedir(join(realpath(__file__), 'src', 'mysql'))
 
+with open(config.config_file) as f:
+    conf = dict(l.strip().split('=', 1) for l in f.readlines() if l.strip()[0] != '#')
+db_login = conf['db_login']
+db_passw = conf['db_password']
+db_port = conf['db_port']
+db_server = conf['db_server']
+
 try:
     cnx = connector.connect(
-        user='orthomcl',
-        password='1234',
-        host='127.0.0.1',
-        port='3307',
+        user=db_login,
+        password=db_passw,
+        host=db_server,
+        port=db_port,
         database='orthomcl',
         buffered=True)
 except:

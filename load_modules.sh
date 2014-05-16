bf6f9ee8cdd455abadf32dbc894f7e4d35ada66f
vladsaveliev <vladsaveliev@me.com>
2014-05-17 00:59:12 +0400

    load modules

--- load_modules.sh
+++ load_modules.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 module load blast
 module load mcl
-
-module unload python
-module load python/64_2.7.3
-
-module unload perl
-module load perl
+module unload python; module load python/64_2.7.3
+module unload perl; module load perl
 export PERL5LIB=/opt/az/perlfoundation/perl/5.16/lib/site_perl/5.16.0/x86_64-linux-thread-multi-ld/:$PERL5LIB
+setenv PERL5LIB /opt/az/perlfoundation/perl/5.16/lib/site_perl/5.16.0/x86_64-linux-thread-multi-ld/:$PERL5LIB
